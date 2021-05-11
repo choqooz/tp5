8d25c7753229e4ff6a993ebf4b516fae14436fd0
choqo <choqo@ChocolatePuma>
2021-05-10 22:14:58 -0300

    creé la clase compra

---
+++ Estrada_Rodolfo_3987_TP4/src/main/java/ar/edu/unju/fi/tp4/models/Compra.java
@@ -0,0 +1,48 @@
+package ar.edu.unju.fi.tp4.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Compra {
+	private String id;
+	private Producto producto;
+	private int cantidad;
+	private double total;
+	public Compra(String id, Producto producto, int cantidad, double total) {
+		super();
+		this.id = id;
+		this.producto = producto;
+		this.cantidad = cantidad;
+		this.total = total;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Producto getProducto() {
+		return producto;
+	}
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	@Override
+	public String toString() {
+		return "Compra [id=" + id + ", producto=" + producto + ", cantidad=" + cantidad + ", total=" + total + "]";
+	}
+	
+	
+}
